0d624bf2aac9075579d489692ad8b088d6fab527
Ardyansyah Vira B <54975146+ardyansyah5711@users.noreply.github.com>
2021-02-24 21:35:37 +0700

    Modul2

---
+++ like_button.js
@@ -0,0 +1,15 @@
+// .... the starter code you pasted ...
+const e = React.createElement;
+
+function LikeButton() {
+    return e(
+        'button',
+        {
+            onClick: () => alert('berhasil')
+        },
+        'Like'
+    );
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
